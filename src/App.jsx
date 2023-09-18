e3304b81b2556e6103423f9c759732c8445090ce
Sumaiyakhan322 <sumaiya.khan32666@gmail.com>
2023-09-19 01:59:34 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+
+import './App.css'
+
+function App() {
+  console.log('Helooo');
+  return (
+    <>
+      <p>hi sumiya</p>
+      <h1 className='text-yellow-500 text-2xl font-bold'>Sumiyta khanannnn</h1>
+      <p className='text-pink-400'>Jayed lkhqan</p>
+      <p className='text-5xl text-red-800 font-bold text-left'> ;Sumaiya khan</p>
+     <ul className='text-left mt-32'>
+      <li className='font-bold'>li</li>
+      <li>ytfh</li>
+     </ul>
+    </>
+  )
+}
+
+export default App
